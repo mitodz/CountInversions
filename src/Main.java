a00744006806b6b84136ec5c4e71c6e55ef979a8
mitodz <mitodzzz@gmail.com>
2019-02-11 17:46:09 +0300

    Казалось, что работает, а нет..

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,30 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-	Scanner scanner = new Scanner(System.in).useDelimiter("\\W");
-	int n = scanner.nextInt(); // число элементов входного массива
-        int[] a = new int[n];
-        for (int i : a) {
-            a[i]=scanner.nextInt();
+        Scanner scanner = new Scanner(System.in).useDelimiter("\\W");
+        int n = scanner.nextInt(); // число элементов входного массива
+        if (n == 1) {
+            System.out.println(0);
+        } else {
+            int count = 0; //количество инверсий
+            List<Integer> list = new ArrayList<>();
+            while (scanner.hasNextInt()) {
+                list.add(scanner.nextInt());
+            }
+            for (int i = 0, k = 1; i < n - 1 && k < n; ) {
+                if (list.get(i) >= list.get(k)) {
+                    count++;
+                    k++;
+                    continue;
+                }
+                i++;
+                k++;
+            }
+            System.out.println(count);
         }
-
     }
 }
