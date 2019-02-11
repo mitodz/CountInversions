99d027cdbe0dade068bcf9632440a383f628cdd2
mitodz <mitodzzz@gmail.com>
2019-02-11 22:07:08 +0300

    Реализована сортировка слиянием

--- src/Main.java
+++ src/Main.java
@@ -5,25 +5,22 @@ public class Main {
     public static int[] mergeArrays(int[] a1, int[] a2) {
         int[] a3 = new int[a1.length + a2.length];
         //outer:
-        int i=0, j=0;
-        for (int k=0; k<a3.length; k++) {
+        int i = 0, j = 0;
+        for (int k = 0; k < a3.length; k++) {
 
-            if (i > a1.length-1) {
+            if (i > a1.length - 1) {
                 int a = a2[j];
                 a3[k] = a;
                 j++;
-            }
-            else if (j > a2.length-1) {
+            } else if (j > a2.length - 1) {
                 int a = a1[i];
                 a3[k] = a;
                 i++;
-            }
-            else if (a1[i] < a2[j]) {
+            } else if (a1[i] < a2[j]) {
                 int a = a1[i];
                 a3[k] = a;
                 i++;
-            }
-            else {
+            } else {
                 int b = a2[j];
                 a3[k] = b;
                 j++;
@@ -34,30 +31,31 @@ public class Main {
     }
 
 
-    public static void showInvariations (int[] ab) {
+    public static void showInvariations(int[] ab) {
         Deque<int[]> q = new ArrayDeque<>();
         for (int i = 0; i < ab.length; i++) {
             q.addLast(new int[]{ab[i]});
         }
-        while (q.size()>1) {
-            q.addLast(mergeArrays(q.pollFirst(),q.pollFirst()));
+        while (q.size() > 1) {
+            q.addLast(mergeArrays(q.pollFirst(), q.pollFirst()));
         }
-        q.forEach(x-> System.out.println(Arrays.toString(x)));
+        q.forEach(x -> System.out.println(Arrays.toString(x)));
     }
-        public static void main (String[]args){
-            Scanner scanner = new Scanner(System.in);
-            int n = scanner.nextInt(); // число элементов входного массива
 
-            if (n == 1) {
-                System.out.println(0);
-            } else {
-                int count = 0; //количество инверсий
-                int[] a = new int[n];
-                for (int i = 0; i < n; i++) {
-                    a[i] = scanner.nextInt();
-                }
-                showInvariations(a);
-//            System.out.println(count);
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt(); // число элементов входного массива
+
+        if (n == 1) {
+            System.out.println(0);
+        } else {
+            int count = 0; //количество инверсий
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++) {
+                a[i] = scanner.nextInt();
             }
+            showInvariations(a);
+//            System.out.println(count);
         }
+    }
 }
